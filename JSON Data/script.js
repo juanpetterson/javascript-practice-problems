ba8f6ced29f88305b4e3e970d85b0673dbde8b29
juanpetterson <juan.petterson@gmail.com>
2020-05-03 22:15:54 -0300

    Working with JSON structure

---
+++ JSON Data/script.js
@@ -0,0 +1,19 @@
+// TASK:
+// 1. Describe what JSON format is.
+// 2. Delete the data types not permitted in JSON.
+// 3. Replace placeholder-text with the corresponding data type,
+//    properly formatted as JSON.
+
+const myJsonObj = {
+  "myString": "some string",
+  "myNumber": 12345.6789,
+  "myNull": null,
+  "myBoolean": false,
+  "myArray": [20, 30, "orange"],
+  "myObject": {
+    "name": 'Juan',
+    "age": 27,
+  },
+};
+
+console.log(myJsonObj);
