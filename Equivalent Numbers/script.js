e2127083d7e8c68275e69bec71d7bd1c40239ee3
juanpetterson <juan.petterson@gmail.com>
2020-05-04 19:36:31 -0300

    Working with numbers equivalence

---
+++ Equivalent Numbers/script.js
@@ -0,0 +1,8 @@
+console.log(300.3 * 3);
+console.log(900.9 === 300.3 * 3);
+
+// const number = Number((300.3 * 3).toFixed(2));
+// const number = Number((300.3 * 3).toPrecision(12));
+const number = (300.3 * 10 * 3) / 10;
+
+console.log(900.9 === number);
